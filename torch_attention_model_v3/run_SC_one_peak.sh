ef0248090e4eb0a3fb3c340a83c44675b8c7a4e7
Krishnan <kraghavan@thetalogin4.tmi.alcf.anl.gov>
2021-10-06 13:43:18 +0000

    nuclear code run

---
+++ torch_attention_model_v3/run_SC_one_peak.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#!/bin/bash
+
+export http_proxy=http://proxy.tmi.alcf.anl.gov:3128
+export https_proxy=http://proxy.tmi.alcf.anl.gov:3128
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/lus/theta-fs0/software/thetagpu/conda/2021-06-26/mconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/lus/theta-fs0/software/thetagpu/conda/2021-06-26/mconda3/etc/profile.d/conda.sh" ]; then
+        . "/lus/theta-fs0/software/thetagpu/conda/2021-06-26/mconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/lus/theta-fs0/software/thetagpu/conda/2021-06-26/mconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+
+conda activate torchRL
+
+
+# mkdir one_peak_test
+python uq_v2.py 
+
+# --peak "one_peak"  --n_points 450000 --json_file "One_peak.json"  --output_model "one_peak_test/"  --input_flag 1  --output_flag 1 --factor_reset 10 --n_iterations  20
+source deactivate
